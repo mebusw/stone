324296c94f65e0f590d9a233bf8b0961f912359b
mebusw <mebusw@163.com>
2014-02-24 08:26:29 +0800

    add landing page and search page

---
+++ stone/SearchViewController.h
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.h
+//  stone
+//
+//  Created by Jacky Shen on 14-2-24.
+//  Copyright (c) 2014年 JackyShen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UITableViewController
+
+
+- (IBAction) backToLandingPage: (id)sender;
+
+@end
